9ede9eab288b82909fea940711b1ff9d09e60410
brootroot <seanlbray@gmail.com>
2017-08-31 20:45:16 -0400

    changed travis.yml filename to .travis.yml

--- config.js
+++ config.js
@@ -1,5 +1,8 @@
 exports.DATABASE_URL =
   process.env.DATABASE_URL ||
   global.DATABASE_URL ||
+  'mongodb://localhost/blog-app';
+exports.TEST_DATABASE_URL =
+  process.env.TEST_DATABASE_URL ||
   'mongodb://test-user:test-pass@ds137360.mlab.com:37360/blog-app';
 exports.PORT = process.env.PORT || 8080;
